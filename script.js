ce2e875a77d05794a17f8e9db5d3c683efb4748f
Mukelani <studentzero68@gmail.com>
2026-06-15 15:28:43 +0200

    sorting the students from the highest to the lowest score

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ function calculateAverage(students) {
 
   return total / students.length;
 }
+
+// Sort students from highest score to lowest score
+// Using Bubble Sort because sort() is not allowed
+function sortStudents(students) {
+  const sortedStudents = [...students];
+
+  for (let i = 0; i < sortedStudents.length - 1; i++) {
+    for (let j = 0; j < sortedStudents.length - 1 - i; j++) {
+      if (sortedStudents[j].score < sortedStudents[j + 1].score) {
+        let temp = sortedStudents[j];
+        sortedStudents[j] = sortedStudents[j + 1];
+        sortedStudents[j + 1] = temp;
+      }
+    }
+  }
+
+  return sortedStudents;
+}
